c34abb36d11d498fa1a7e28ff09fc7acf46eddc2
Maxim Gubanov <m.gubanov@tehnomer.ru>
2025-10-01 16:22:17 +0300

    создан проект

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/MaximGubanov/aup-config"
+)
+
+func main() {
+	execPath, _ := os.Executable()
+	execDir := filepath.Dir(execPath)
+	cfg, err := aup_config.NewConfig("../sgs.json", execDir)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
+}
---
+++ config.go
@@ -0,0 +1,76 @@
+﻿package aup_config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type service struct {
+	Endpoint struct {
+		Host string `json:"Host"`
+		Port int    `json:"Port"`
+	} `json:"Endpoint"`
+	LogLevel string `json:"LogLevel"`
+}
+
+type Config struct {
+	Services struct {
+		Watcher    service `json:"WatcherService"`
+		Validator  service `json:"ValidatorService"`
+		DbWriter   service `json:"DbWriterService"`
+		SMTHandler service `json:"SMTHandler"`
+	} `json:"GrpcServer"`
+	Directories struct {
+		LogDir []string `json:"LogDir"`
+		AupDir []string `json:"AupDir"`
+	} `json:"Directories"`
+	execDir string
+}
+
+func NewConfig(filename, execDir string) (*Config, error) {
+	data, err := os.ReadFile(filepath.Join(execDir, filename))
+	if err != nil {
+		return nil, errors.New("ошибка открытия конфигурационного файла")
+	}
+
+	var config Config
+	config.execDir = execDir
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, errors.New("ошибка преобразования конфигурационного файла из json")
+	}
+
+	return &config, nil
+}
+
+func (c *Config) GetLogDir() string {
+	logDir := filepath.Join(c.execDir, "../", c.Directories.LogDir[2], "/")
+	return logDir
+}
+
+func (c *Config) GetArcInDir() string {
+	arcInDir := filepath.Join(c.execDir, "../", c.Directories.AupDir[2], c.Directories.AupDir[3], "/")
+	return arcInDir
+}
+
+func (c *Config) GetOkDir() (string, error) {
+	okDir := filepath.Join(c.execDir, "../", c.Directories.AupDir[2], c.Directories.AupDir[3], "OK", "/")
+	if okDir == "" {
+		return "", fmt.Errorf("не существующий путь: %s", "Arc/In/OK/")
+	}
+
+	return okDir, nil
+}
+
+func (c *Config) GetOwDir() (string, error) {
+	owDir := filepath.Join(c.execDir, "../", c.Directories.AupDir[2], c.Directories.AupDir[3], "OW", "/")
+	if owDir == "" {
+		return "", fmt.Errorf("не существующий путь: %s", "Arc/In/OW/")
+	}
+
+	return owDir, nil
+}
